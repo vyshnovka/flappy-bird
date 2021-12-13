a9e5eff5c0577dd4ea6e1224888532002b8f0f90
Viktoria Zheneral <viktoria.zheneral@gmail.com>
2021-12-13 15:29:42 +0200

    variable renamed

--- Assets/Scripts/ObstacleSpawn.cs
+++ Assets/Scripts/ObstacleSpawn.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 public class ObstacleSpawn : MonoBehaviour
 {
-    public float currentTime;
-    public float timer = 0;
+    private float timer;
+    public float deltaTime;
+   
     public GameObject obstacle;
     public float range;
 
     void Update()
     {
-        if (timer > currentTime)
+        if (timer > deltaTime)
         {
             GameObject obstacleClone = Instantiate(obstacle);
             obstacleClone.transform.position = transform.position + new Vector3(0, Random.Range(-range, range + 0.6f), 0);
