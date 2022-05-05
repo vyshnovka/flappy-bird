c20e73e4588a2f06026a64257b7a42c017229a93
Vykula <viktoria.zheneral@gmail.com>
2022-05-05 22:33:20 +0300

    rearrangement

--- Assets/Scripts/GUI/MonologueManager.cs
+++ Assets/Scripts/GUI/MonologueManager.cs
@@ -7,8 +7,16 @@ public class MonologueManager : MonoBehaviour
     [SerializeField]
     private GameObject playButton;
 
+    [SerializeField]
+    private GameObject bubble;
+
     public void StartMonologue()
     {
         playButton.SetActive(false);
+
+        StartCoroutine(Utility.TimedEvent(() =>
+        {
+            bubble.SetActive(true);
+        }, 2f));
     }
 }
