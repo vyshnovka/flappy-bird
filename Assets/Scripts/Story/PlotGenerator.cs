6ffaef1822d907152dfa3a7dc5d931338896d402
Vykula <viktoria.zheneral@gmail.com>
2022-05-06 22:05:14 +0300

    test plot added

--- Assets/Scripts/Story/PlotGenerator.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Plot", menuName = "ScriptableObjects/Plot")]
-public class PlotGenerator : ScriptableObject
-{
-    public List<string> plot;
-}
