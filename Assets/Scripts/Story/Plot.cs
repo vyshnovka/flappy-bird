75060dd46e630e5ab27dfada59b73c11fdf3bc00
Vykula <viktoria.zheneral@gmail.com>
2022-05-06 22:06:03 +0300

    plot finished

---
+++ Assets/Scripts/Story/Plot.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Plot", menuName = "ScriptableObjects/Plot")]
+public class Plot : ScriptableObject
+{
+    public List<string> lines;
+}
