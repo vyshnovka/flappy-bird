64a536b70d56122b2b15c5ff59b1b8f1465288a7
Vykula <viktoria.zheneral@gmail.com>
2022-05-07 02:06:53 +0300

    audio controller added

---
+++ Assets/Scripts/GUI/AudioController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    public static AudioController instance;
+
+    private AudioSource audioSource;
+
+    [SerializeField]
+    private AudioClip tapSound;
+    [SerializeField]
+    private AudioClip deathSound;
+
+    void Awake()
+    {
+        if (instance)
+        {
+            Destroy(instance);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayTap()
+    {
+        audioSource.PlayOneShot(tapSound, 1f);
+    }
+
+    public void PlayDeath()
+    {
+        audioSource.PlayOneShot(deathSound, 1f);
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioSource.volume = volume;
+    }
+}
